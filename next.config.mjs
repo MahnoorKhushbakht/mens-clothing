0d76ebb9663ecb8bbc69cae7034a50ae38954272
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-06-05 22:26:51 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'tg-clothing.000webhostapp.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+    images: {
+      domains: ['tg-clothing.000webhostapp.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
