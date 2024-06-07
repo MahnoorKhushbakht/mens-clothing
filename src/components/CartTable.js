f33dccd98cd653a59f25082b0edf57e6397cc0dd
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-06-07 21:01:39 +0500

    updated

--- src/components/CartTable.js
+++ src/components/CartTable.js
@@ -68,7 +68,7 @@ export default function CartTable({ userId }) {
        <div className='flex justify-center'>
   <h1>Cart</h1>
 </div>
-          <Table variant='striped' colorScheme='black' className='w-80'>
+          <Table variant='simple' colorScheme='black' className='w-80' size={{ base: 'sm', md: 'md', lg: 'lg' }}>
             <Thead>
               <Tr>
                 <Th>Product Name</Th>
