49238c862cc34123de443c9a9fafbdd9f17a2b5e
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-06-09 17:28:03 +0500

    updated

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -15,12 +15,12 @@ export default function Catalog() {
       <p className="text-center text-gray-800 text-lg italic mb-6 w-80 md:w-full font-bold">
       Explore now to elevate your wardrobe!
       </p>
-      <ul className='grid grid-cols-2 md:grid-cols-3 gap-4 '>
+      <ul className='grid grid-cols-1 md:grid-cols-3 gap-4 '>
         {category.map((categoryItem,index) => (
           <li key={index}>
             <Link href={`/category/${categoryItem.slug}`} style={{ textDecoration: 'none' }}>
               <Paper 
-                className=' drop-shadow-sm w-32 h-24 flex items-center justify-center bg-gray-800 text-gray-200 hover:bg-gray-900 disabled:bg-slate-500' 
+                className=' drop-shadow-sm md:w-32 w-40 h-32 md:h-24 flex items-center justify-center bg-gray-800 text-gray-200 hover:bg-gray-900 disabled:bg-slate-500' 
                 elevation={3} 
               >
                 {categoryItem.name}
